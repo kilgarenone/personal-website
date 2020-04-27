7d42f1dc125eb5aa31503f4cbc412095e95a8981
Kheoh Yee Wei <oldjoy@protonmail.com>
2020-04-27 11:03:33 +0800

    Add rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+
   eleventyConfig.addFilter("dateIso", (date) => {
     return new Date(date).toISOString();
   });
