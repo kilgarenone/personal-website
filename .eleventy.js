949eeea76003f4a3093772eebb2648af1de922a0
Kheoh Yee Wei <oldjoy@protonmail.com>
2020-05-06 08:46:12 +0800

    RSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    // Or, just njk and md syntax highlighters (do not install liquid)
+    templateFormats: ["njk", "md"],
+
+    // alwaysWrapLineHighlights: false
+  });
+
   eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.addFilter("dateIso", (date) => {
